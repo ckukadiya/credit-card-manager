f89d1868a3a0833f80e522e5c23bd9a5bf614cec
Chintan <cs.rajipo@gmail.com>
2024-08-05 07:11:58 +0530

    initial commit

---
+++ handler.js
@@ -0,0 +1,87 @@
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+// Simulate a user database for authentication
+const users = [{ username: "user1", password: "password1" }];
+
+exports.login = async (event) => {
+  const { username, password } = JSON.parse(event.body);
+
+  // Verify username and password
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
+  if (!user) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ message: "Invalid credentials" }),
+    };
+  }
+
+  // Generate JWT token
+  const token = jwt.sign({ username }, JWT_SECRET, { expiresIn: "1h" });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ token }),
+  };
+};
+
+exports.getData = async (event) => {
+  // Token is validated by the custom authorizer
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ data: "Protected data" }),
+  };
+};
+
+exports.getToken = async (event) => {
+  const {
+    CREDIT_SERVICE_URL,
+    CREDIT_SERVICE_USERNAME,
+    CREDIT_SERVICE_PASSWORD,
+  } = process.env;
+
+  try {
+    const response = await axios.post(CREDIT_SERVICE_URL, {
+      username: CREDIT_SERVICE_USERNAME,
+      password: CREDIT_SERVICE_PASSWORD,
+    });
+    console.log(response.data);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.validateToken = async (event) => {
+  console.log(event);
+  const token = event.headers ? event.headers.Authorization : undefined;
+  if (!token) {
+    throw new Error("Unauthorized");
+  }
+  try {
+    jwt.verify(token.replace("Bearer ", ""), JWT_SECRET);
+    return {
+      principalId: "user",
+      policyDocument: {
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Action: "execute-api:Invoke",
+            Effect: "Allow",
+            Resource: event.methodArn,
+          },
+        ],
+      },
+    };
+  } catch (error) {
+    throw new Error("Unauthorized");
+  }
+};
